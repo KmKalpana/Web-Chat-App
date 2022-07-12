8df870986d1450a6a9eff42aace3469e0fe1125f
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-07-12 18:51:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/user',userRoutes);
 app.use('/api/chats',chatRoutes);
 app.use(Notfound);
 app.use(ErrorHandler);
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5600;
 app.listen(PORT,(()=>{
     console.log(`server successfully started on Port ${PORT}`);
 }))
